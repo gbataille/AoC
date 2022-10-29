404b2be799775ba3096da1d0380d0828e4a7180a
Grégory Bataille <gregory.bataille@gmail.com>
2022-10-29 16:45:52 +0200

    feat: run with problem input

---
+++ y2021d5/main.go
@@ -0,0 +1,23 @@
+package y2021day5
+
+import "fmt"
+
+func main() {
+  fmt.Println("Day 5")
+}
+
+// Part1 solves the Part 1 of the problem
+func Part1(inputData string) (string, error) {
+  var res string
+  fmt.Println("Running day 5 Part 1")
+
+  return res, nil
+}
+
+// Part2 solves the Part 2 of the problem
+func Part2(inputData string) (string, error) {
+  var res string
+  fmt.Println("Running day 5 Part 2")
+
+  return res, nil
+}
