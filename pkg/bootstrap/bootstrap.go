dec4f3dd7c41c6c4418bdd1579d3863218fee531
Grégory Bataille <gregory.bataille@gmail.com>
2022-10-29 08:21:45 +0200

    feat: generation main file

--- pkg/bootstrap/bootstrap.go
+++ pkg/bootstrap/bootstrap.go
@@ -2,6 +2,7 @@ package bootstrap
 
 import (
 	"fmt"
+	"html/template"
 	"os"
 	"path"
 	"strconv"
@@ -10,22 +11,64 @@ import (
 func InitializeDay(day uint64) error {
 	fmt.Printf("Initializing day %v\n", day)
 
-	createFolder(day)
+	folder, err := createFolder(day)
+	if err != nil {
+		return err
+	}
+
+	err = createMain(folder)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
 
-func createFolder(day uint64) error {
+func createFolder(day uint64) (string, error) {
 	dirName := fmt.Sprintf("day%v", strconv.FormatUint(day, 10))
 	fmt.Printf("Creating the day's directory %v\n", dirName)
 
 	curDir, err := os.Getwd()
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	dayDir := path.Join(curDir, dirName)
 	err = os.Mkdir(dayDir, os.ModePerm)
+	if err != nil {
+		return "", err
+	}
+
+	return dayDir, nil
+}
+
+func createMain(folderPath string) error {
+	fmt.Println("Creating the day's main file")
+
+	const TemplateName = "day.main.go.tmpl"
+
+	workDir, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	templatePath := path.Join(workDir, "assets", TemplateName)
+
+	tmpl, err := template.ParseFiles(templatePath)
+	if err != nil {
+		return err
+	}
+	fmt.Println(tmpl.Name())
+
+	outFilePath := path.Join(folderPath, "main.go")
+	outFile, err := os.Create(outFilePath)
+	if err != nil {
+		return err
+	}
+	defer outFile.Close()
+
+	err = tmpl.Execute(outFile, struct {
+		DayNumber int
+	}{DayNumber: 5})
 	if err != nil {
 		return err
 	}
