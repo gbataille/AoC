acc7c88199cefc3bc423ed651b72543945d8148a
Grégory Bataille <gregory.bataille@gmail.com>
2022-12-17 07:59:59 +0100

    d14 part 1

--- rust/problems/y2022/d14/src/main.rs
+++ rust/problems/y2022/d14/src/main.rs
@@ -1,7 +1,97 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
+use std::error::Error;
 use std::fs;
 use std::str::FromStr;
 use std::vec::Vec;
+
+struct World {
+    rocks_coord: HashSet<(i32, i32)>,
+    moving_sand_coord: (i32, i32),
+    sands_coord: HashSet<(i32, i32)>,
+    min_x: i32,
+    max_x: i32,
+    min_y: i32,
+    max_y: i32,
+}
+
+impl World {
+    fn new() -> Self {
+        World {
+            rocks_coord: HashSet::new(),
+            moving_sand_coord: (500, 0),
+            sands_coord: HashSet::new(),
+            min_x: std::i32::MAX,
+            max_x: 0,
+            min_y: 0,
+            max_y: 0,
+        }
+    }
+
+    fn add_rock(&mut self, coord: (i32, i32)) {
+        self.rocks_coord.insert(coord);
+        if coord.0 < self.min_x {
+            self.min_x = coord.0;
+        }
+        if coord.0 > self.max_x {
+            self.max_x = coord.0;
+        }
+        if coord.1 < self.min_y {
+            self.min_y = coord.1;
+        }
+        if coord.1 > self.max_y {
+            self.max_y = coord.1;
+        }
+    }
+
+    fn is_occupied(&self, coord: (i32, i32)) -> bool {
+        self.rocks_coord.get(&coord).is_some() || self.sands_coord.get(&coord).is_some()
+    }
+
+    fn is_out(&self, coord: (i32, i32)) -> bool {
+        coord.0 < self.min_x || coord.0 > self.max_x || coord.1 > self.max_y || coord.1 < self.min_y
+    }
+
+    fn move_step(&mut self) -> Result<(), Box<dyn Error>> {
+        for attempt in [
+            (self.moving_sand_coord.0, self.moving_sand_coord.1 + 1),
+            (self.moving_sand_coord.0 - 1, self.moving_sand_coord.1 + 1),
+            (self.moving_sand_coord.0 + 1, self.moving_sand_coord.1 + 1),
+        ] {
+            if self.is_out(attempt) {
+                return Err(format!("Out at {:?}", attempt))?;
+            }
+            if !self.is_occupied(attempt) {
+                self.moving_sand_coord = attempt;
+                return Ok(());
+            }
+        }
+
+        // did not move, so settled
+        self.sands_coord.insert(self.moving_sand_coord);
+        self.moving_sand_coord = (500, 0);
+
+        Ok(())
+    }
+
+    fn display(&self) {
+        for y in self.min_y..=self.max_y {
+            for x in self.min_x..=self.max_x {
+                if y == 0 && x == 500 {
+                    print!("+");
+                } else if x == self.moving_sand_coord.0 && y == self.moving_sand_coord.1 {
+                    print!("o");
+                } else if self.sands_coord.get(&(x, y)).is_some() {
+                    print!("O");
+                } else if self.rocks_coord.get(&(x, y)).is_some() {
+                    print!("#");
+                } else {
+                    print!(".");
+                }
+            }
+            println!("");
+        }
+    }
+}
 
 fn main() {
     let contents = fs::read_to_string("input.txt").expect("Should have been able to read the file");
@@ -10,9 +100,64 @@ fn main() {
     part2(&contents);
 }
 
-fn part1(contents: &String) {}
+fn part1(contents: &String) {
+    let mut world = World::new();
+    for line in contents.lines() {
+        parse_line(line, &mut world);
+    }
+
+    world.display();
+
+    let mut step = 1;
+    loop {
+        println!("Step {}", step);
+
+        match world.move_step() {
+            Ok(_) => (),
+            Err(a) => {
+                println!("End with: {}", a);
+                break;
+            }
+        }
+
+        // world.display();
+
+        step += 1;
+    }
+
+    world.display();
+    println!("\nNb settled sand {}", world.sands_coord.len());
+}
 
 fn part2(contents: &String) {}
+
+fn parse_line(line: &str, world: &mut World) {
+    let mut corners: Vec<(i32, i32)> = Vec::new();
+
+    for corner in line.split(" -> ") {
+        let coords = corner.split(",").collect::<Vec<&str>>();
+        let (x_str, y_str) = (coords[0], coords[1]);
+        let x = x_str.parse::<i32>().unwrap();
+        let y = y_str.parse::<i32>().unwrap();
+        corners.push((x, y));
+    }
+
+    for i_corner in 1..corners.len() {
+        let a = corners[i_corner - 1];
+        let b = corners[i_corner];
+
+        let from_x = a.0.min(b.0);
+        let to_x = a.0.max(b.0);
+        let from_y = a.1.min(b.1);
+        let to_y = a.1.max(b.1);
+
+        for i_x in from_x..=to_x {
+            for i_y in from_y..=to_y {
+                world.add_rock((i_x, i_y));
+            }
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -21,15 +166,8 @@ mod tests {
     #[test]
     fn test_parts() {
         let test_contents = String::from_str(
-            "    [D]    
-[N] [C]    
-[Z] [M] [P]
- 1   2   3 
-
-move 1 from 2 to 1
-move 3 from 1 to 3
-move 2 from 2 to 1
-move 1 from 1 to 2",
+            "498,4 -> 498,6 -> 496,6
+503,4 -> 502,4 -> 502,9 -> 494,9",
         )
         .unwrap();
 
